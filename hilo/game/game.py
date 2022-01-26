56a1dc2ab2431846bf0c1807216bf69784cde4aa
rodrigoloyo <rmloyo@byui.edu>
2022-01-26 09:18:16 -0600

    game update 1/26

--- hilo/game/game.py
+++ hilo/game/game.py
@@ -1,13 +1,16 @@
-# from game.gameOver import  #pending
 import random
 
+
 class Game:
-    """The blueprint who directs the game. 
+    """A person who directs the game. 
     
     The responsibility of a Game is to control the sequence of play.
 
     Attributes:
-        
+        dice (List[Die]): A list of Die instances.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
     """
 
     def __init__(self):
@@ -17,14 +20,15 @@ class Game:
             self (Game): an instance of Game.
         """
         self.card = list(range(1,13))
-        self.guess = None
+        self.new_card = 0
+        self.guess = 0
         self.is_playing = True
         self.score = 300
         self.total_score = 0
 
          
-        # game_over = Over()
-        # self.card.append(game_over)
+        # die = Die()
+        # self.card.append(die)
 
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -36,16 +40,18 @@ class Game:
             self.display_card()
             self.get_inputs()
             self.display_card()
-            # self.do_updates()
-            # self.do_outputs()
+            self.do_updates()
+            self.do_outputs()
             self.get_inputs1()
+
     def display_card(self):
 
-        new_card = self.card.pop(random.randrange(len(self.card)))
-        print(new_card)
+        new_card = int(self.card.pop(random.randrange(len(self.card))))
+        return new_card
+        
 
     def get_inputs(self):
-        """Ask the user to guess next card if higher or lower
+        """Ask the user if they want to roll.
 
         Args:
             self (Game): An instance of Game.
@@ -55,9 +61,66 @@ class Game:
        
 
         self.guess = guess_card
-        
-    def get_inputs1(self):
-        new_round = input("Play again? [y/n] ")
-        self.is_playing = (new_round == "y")
+        print(self.guess)
+    def display_card(self):
+
+        self.current_card = self.card.pop(random.randrange(len(self.card)))
+        print(self.current_card)
+        print(type(self.new_card))
 
     
+
+    def get_inputs1(self):
+        new_round = input("Play again [y/n] ")
+        self.is_playing = (new_round == "y")
+
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Game): An instance of Game.
+        """
+        # if not self.is_playing:
+        #     return 
+
+        # for i in range(len(self.dice)):
+        #     die = self.dice[i]
+        #     die.roll()
+        #     self.score += die.points 
+        # self.total_score += self.score
+        print(type(self.card))
+         
+        new_card = self.display_card()
+        print(type(self.current_card))
+        print("test")
+        print(type(new_card))
+        # print(type(self.new_card, type(self.current_card)))
+
+        if self.new_card > self.current_card and self.guess == "h":
+
+          self.score += 100
+        else:
+            self.score -= 100
+        print(self.score)
+
+     
+
+
+
+    def do_outputs(self):
+        """Displays the dice and the score. Also asks the player if they want to roll again. 
+
+        Args:
+            self (Game): An instance of Game.
+        """
+        if not self.is_playing:
+            return
+        
+        # values = ""
+        # for i in range(len(self.dice)):
+        #     die = self.dice[i]
+        #     values += f"{die.value} "
+
+        # print(f"You rolled: {values}")
+        print(f"Your score is: {self.score}\n")
+        self.is_playing == (self.score > 0)
